e49b9bfa4ce0a64b433960af93db893efb17ccd5
asu1977 <asu1977@gmail.com>
2017-02-26 13:45:47 +0300

    MenuBar

--- src/FirstGUI.scala
+++ src/FirstGUI.scala
@@ -1,8 +1,42 @@
 import scala.swing._
 
+val textArea = new TextArea
+
+def openFile: Unit ={
+  val chooser = new FileChooser
+  if (chooser.showOpenDialog(null)==FileChooser.Result.Approve){
+    val source = scala.io.Source.fromFile(chooser.selectedFile)
+    textArea.text = source.mkString
+    source.close()
+  }
+}
+
+def saveFile: Unit = {
+  val chooser = new FileChooser
+  if (chooser.showSaveDialog(null)==FileChooser.Result.Approve){
+    val pw = new java.io.PrintWriter(chooser.selectedFile)
+    pw.print(textArea.text)
+    pw.close
+  }
+}
+
 val frame = new MainFrame {
   title = "My First GUI"
-  contents = Button("Click Me!")(println("Booton was clicked."))
+  contents = textArea
+  menuBar = new MenuBar {
+    contents += new Menu("File"){
+      contents += new MenuItem(Action("Open"){
+        openFile
+      })
+      contents += new MenuItem(Action("Save"){
+        saveFile
+      })
+      contents += new Separator
+      contents += new MenuItem(Action("Exit"){
+        sys.exit(0)
+      })
+    }
+  }
   size = new Dimension(500, 500)
   centerOnScreen
 }
